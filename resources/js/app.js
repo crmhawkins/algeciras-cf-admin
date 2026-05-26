b8915c7256caf36d6e467bf5fd30c12fc6f14bc3
Ivan Garcia <ivan@algecirasclubdefutbol.com>
2026-05-26 10:01:36 +0200

    fix(fx): quitar import Alpine (Livewire 4 ya lo trae); expose gsap/ScrollTrigger global

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,14 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
-window.Alpine = Alpine;
-Alpine.start();
+// NOTA: NO importar Alpine. Livewire 4 ya trae Alpine integrado y lo inicializa.
+// Si lo importamos aparece warning "Detected multiple instances of Alpine running"
+// que rompe Livewire (cart counter no refresca, etc).
 
 // Motion FX (parallax, reveal, counters, tilt...)
 import './effects.js';
+
+// Exponer GSAP global para debug
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+window.gsap = gsap;
+window.ScrollTrigger = ScrollTrigger;
